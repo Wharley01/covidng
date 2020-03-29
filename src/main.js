c3703982e0dae6713c5c67f27d54183a7128d875
Wharley01 <hackerslord96@gmail.com>
2020-03-29 10:48:55 +0100

    needed libararies imported

--- src/main.js
+++ src/main.js
@@ -1,13 +1,62 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import createStore from "./store";
+import axios from "axios";
+import Meta from "vue-meta";
+import Vuex, { mapState } from "vuex";
 
-Vue.config.productionTip = false
+Vue.use(Meta, {
+  // optional pluginOptions
+  refreshOnceOnNavigation: true
+});
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(Vuex);
+
+// Vue.config.devtools = true;
+
+if (typeof window !== "undefined") {
+}
+
+// let api_root = '';
+let api_root = "https://api.website.com";
+let api_version = "v1";
+
+// import
+
+let axiosInstance = axios.create({
+  baseURL: api_root + "/" + api_version,
+  withCredentials: true
+});
+
+Vue.prototype.$http = axiosInstance;
+Vue.config.productionTip = false;
+
+export default (state = null, router) => {
+  const store = createStore(Vuex, new Vue(), router);
+
+  router.beforeEach((to, from, next) => {
+    ////console.log("Route Loading");
+    next();
+  });
+
+  router.afterEach((to, from) => {
+
+    ////console.log("Route Loaded");
+  });
+
+
+  const app = new Vue({
+    // inject router into root Vue instance
+    router,
+    store,
+    render: h => h(App)
+  });
+
+  // return both the app and the router
+  return {
+    app,
+    router,
+    store,
+    Vue
+  };
+};
--- src/router/server.js
+++ src/router/server.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import routesClient from "./routes-client";
 
 let has_context = typeof context != 'undefined'
+
+import routesServer from '../router/routes-server'
+//import entrance
 
 Vue.use(Router)
 
@@ -10,9 +12,7 @@ export default () => {
 
   return new Router({
     mode: 'history',
-    routes: routesClient,
-    scrollBehavior() {
-      return { x: 0, y: 0 };
-    },
-  })
+    routes: routesServer
+});
+
 }
